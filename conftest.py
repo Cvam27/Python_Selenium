d0cd7e7d176d414e949aa371e9e9a884f03b0dfc
Shivam_Acharya <sracharya27@gmail.com>
2026-03-26 20:30:29 +0530

    configure Chrome WebDriver options for headless execution

--- conftest.py
+++ conftest.py
@@ -1,14 +1,17 @@
 import pytest
+
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
+options = Options()
+options.add_argument("--headless=new")
+options.add_argument("--no-sandbox")
+options.add_argument("--disable-dev-shm-usage")
 
-# options = Options()
-# options.add_argument(argument="--start-maximized")
-# # options.add_argument(argument="--headless=new")
 
 @pytest.fixture
 def driver():
-    driver = webdriver.Chrome()
+    driver = webdriver.Chrome(options=options)
     driver.maximize_window()
     driver.get("https://selectorshub.com/xpath-practice-page/")
     yield driver
