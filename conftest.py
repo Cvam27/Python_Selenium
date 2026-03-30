dd9f75f86b222f7e90654e9490a98e7ee137e27c
Cvam27 <60308566+Cvam27@users.noreply.github.com>
2026-03-30 22:05:52 +0530

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -3,8 +3,7 @@ import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
-options = Options()
-# options.add_argument("--headless=new")
+options = Options() options.add_argument("--headless=new")
 options.add_argument("--no-sandbox")
 options.add_argument("--disable-dev-shm-usage")
 
